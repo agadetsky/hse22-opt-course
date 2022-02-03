ee201685d6023ec7e8913e592ee7f125429c9f35
Artyom <artygadetsky@yandex.ru>
2022-02-03 14:59:41 +0300

    push task1

---
+++ task1/optimization.py
@@ -0,0 +1,122 @@
+from collections import defaultdict, deque  # Use this for effective implementation of L-BFGS
+
+import numpy as np
+
+from utils import get_line_search_tool
+
+
+def gradient_descent(oracle, x_0, tolerance=1e-5, max_iter=10000,
+                     line_search_options=None, trace=False, display=False):
+    """
+    Gradien descent optimization method.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func(), .grad() and .hess() methods implemented for computing
+        function value, its gradient and Hessian respectively.
+    x_0 : np.array
+        Starting point for optimization algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+    display : bool
+        If True, debug information is displayed during optimization.
+        Printing format and is up to a student and is not checked in any way.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        "success" or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+                the stopping criterion.
+            - 'computational_error': in case of getting Infinity or None value during the computations.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['time'] : list of floats, containing time in seconds passed from the start of the method
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+
+    Example:
+    --------
+    >> oracle = QuadraticOracle(np.eye(5), np.arange(5))
+    >> x_opt, message, history = gradient_descent(oracle, np.zeros(5), line_search_options={'method': 'Armijo', 'c1': 1e-4})
+    >> print('Found optimal point: {}'.format(x_opt))
+       Found optimal point: [ 0.  1.  2.  3.  4.]
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement gradient descent
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
+
+
+def newton(oracle, x_0, tolerance=1e-5, max_iter=100,
+           line_search_options=None, trace=False, display=False):
+    """
+    Newton's optimization method.
+
+    Parameters
+    ----------
+    oracle : BaseSmoothOracle-descendant object
+        Oracle with .func(), .grad() and .hess() methods implemented for computing
+        function value, its gradient and Hessian respectively. If the Hessian
+        returned by the oracle is not positive-definite method stops with message="newton_direction_error"
+    x_0 : np.array
+        Starting point for optimization algorithm
+    tolerance : float
+        Epsilon value for stopping criterion.
+    max_iter : int
+        Maximum number of iterations.
+    line_search_options : dict, LineSearchTool or None
+        Dictionary with line search options. See LineSearchTool class for details.
+    trace : bool
+        If True, the progress information is appended into history dictionary during training.
+        Otherwise None is returned instead of history.
+    display : bool
+        If True, debug information is displayed during optimization.
+
+    Returns
+    -------
+    x_star : np.array
+        The point found by the optimization procedure
+    message : string
+        'success' or the description of error:
+            - 'iterations_exceeded': if after max_iter iterations of the method x_k still doesn't satisfy
+                the stopping criterion.
+            - 'newton_direction_error': in case of failure of solving linear system with Hessian matrix (e.g. non-invertible matrix).
+            - 'computational_error': in case of getting Infinity or None value during the computations.
+    history : dictionary of lists or None
+        Dictionary containing the progress information or None if trace=False.
+        Dictionary has to be organized as follows:
+            - history['time'] : list of floats, containing time passed from the start of the method
+            - history['func'] : list of function values f(x_k) on every step of the algorithm
+            - history['grad_norm'] : list of values Euclidian norms ||g(x_k)|| of the gradient on every step of the algorithm
+            - history['x'] : list of np.arrays, containing the trajectory of the algorithm. ONLY STORE IF x.size <= 2
+
+    Example:
+    --------
+    >> oracle = QuadraticOracle(np.eye(5), np.arange(5))
+    >> x_opt, message, history = newton(oracle, np.zeros(5), line_search_options={'method': 'Constant', 'c': 1.0})
+    >> print('Found optimal point: {}'.format(x_opt))
+       Found optimal point: [ 0.  1.  2.  3.  4.]
+    """
+    history = defaultdict(list) if trace else None
+    line_search_tool = get_line_search_tool(line_search_options)
+    x_k = np.copy(x_0)
+
+    # TODO: Implement Newton's method.
+    # Use line_search_tool.line_search() for adaptive step size.
+    return x_k, 'success', history
---
+++ task1/oracles.py
@@ -0,0 +1,179 @@
+import numpy as np
+import scipy
+from scipy.special import expit
+
+
+class BaseSmoothOracle(object):
+    """
+    Base class for implementation of oracles.
+    """
+
+    def func(self, x):
+        """
+        Computes the value of function at point x.
+        """
+        raise NotImplementedError('Func oracle is not implemented.')
+
+    def grad(self, x):
+        """
+        Computes the gradient at point x.
+        """
+        raise NotImplementedError('Grad oracle is not implemented.')
+
+    def hess(self, x):
+        """
+        Computes the Hessian matrix at point x.
+        """
+        raise NotImplementedError('Hessian oracle is not implemented.')
+
+    def func_directional(self, x, d, alpha):
+        """
+        Computes phi(alpha) = f(x + alpha*d).
+        """
+        return np.squeeze(self.func(x + alpha * d))
+
+    def grad_directional(self, x, d, alpha):
+        """
+        Computes phi'(alpha) = (f(x + alpha*d))'_{alpha}
+        """
+        return np.squeeze(self.grad(x + alpha * d).dot(d))
+
+    def hess_vec(self, x, v):
+        """
+        Computes matrix-vector product with Hessian matrix f''(x) v
+        """
+        return self.hess(x).dot(v)
+
+
+class QuadraticOracle(BaseSmoothOracle):
+    """
+    Oracle for quadratic function:
+       func(x) = 1/2 x^TAx - b^Tx.
+    """
+
+    def __init__(self, A, b):
+        if not scipy.sparse.isspmatrix_dia(A) and not np.allclose(A, A.T):
+            raise ValueError('A should be a symmetric matrix.')
+        self.A = A
+        self.b = b
+
+    def func(self, x):
+        return 0.5 * np.dot(self.A.dot(x), x) - self.b.dot(x)
+
+    def grad(self, x):
+        return self.A.dot(x) - self.b
+
+    def hess(self, x):
+        return self.A
+
+
+class LogRegL2Oracle(BaseSmoothOracle):
+    """
+    Oracle for logistic regression with l2 regularization:
+         func(x) = 1/m sum_i log(1 + exp(-b_i * a_i^T x)) + regcoef / 2 ||x||_2^2.
+
+    Let A and b be parameters of the logistic regression (feature matrix
+    and labels vector respectively).
+    For user-friendly interface use create_log_reg_oracle()
+
+    Parameters
+    ----------
+        matvec_Ax : function
+            Computes matrix-vector product Ax, where x is a vector of size n.
+        matvec_ATy : function of y
+            Computes matrix-vector product A^Ty, where y is a vector of size m.
+        matmat_ATsA : function
+            Computes matrix-matrix-matrix product A^T * Diag(s) * A,
+    """
+
+    def __init__(self, matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef):
+        self.matvec_Ax = matvec_Ax
+        self.matvec_ATx = matvec_ATx
+        self.matmat_ATsA = matmat_ATsA
+        self.b = b
+        self.regcoef = regcoef
+
+    def func(self, x):
+        # TODO: Implement
+        return None
+
+    def grad(self, x):
+        # TODO: Implement
+        return None
+
+    def hess(self, x):
+        # TODO: Implement
+        return None
+
+
+class LogRegL2OptimizedOracle(LogRegL2Oracle):
+    """
+    Oracle for logistic regression with l2 regularization
+    with optimized *_directional methods (are used in line_search).
+
+    For explanation see LogRegL2Oracle.
+    """
+
+    def __init__(self, matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef):
+        super().__init__(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+
+    def func_directional(self, x, d, alpha):
+        # TODO: Implement optimized version with pre-computation of Ax and Ad
+        return None
+
+    def grad_directional(self, x, d, alpha):
+        # TODO: Implement optimized version with pre-computation of Ax and Ad
+        return None
+
+
+def create_log_reg_oracle(A, b, regcoef, oracle_type='usual'):
+    """
+    Auxiliary function for creating logistic regression oracles.
+        `oracle_type` must be either 'usual' or 'optimized'
+    """
+    def matvec_Ax(x):
+        # TODO: Implement
+        return None
+
+    def matvec_ATx(x):
+        # TODO: Implement
+        return None
+
+    def matmat_ATsA(s):
+        # TODO: Implement
+        return None
+
+    if oracle_type == 'usual':
+        oracle = LogRegL2Oracle
+    elif oracle_type == 'optimized':
+        oracle = LogRegL2OptimizedOracle
+    else:
+        raise 'Unknown oracle_type=%s' % oracle_type
+    return oracle(matvec_Ax, matvec_ATx, matmat_ATsA, b, regcoef)
+
+
+def grad_finite_diff(func, x, eps=1e-8):
+    """
+    Returns approximation of the gradient using finite differences:
+        result_i := (f(x + eps * e_i) - f(x)) / eps,
+        where e_i are coordinate vectors:
+        e_i = (0, 0, ..., 0, 1, 0, ..., 0)
+                          >> i <<
+    """
+    # TODO: Implement numerical estimation of the gradient
+    return None
+
+
+def hess_finite_diff(func, x, eps=1e-5):
+    """
+    Returns approximation of the Hessian using finite differences:
+        result_{ij} := (f(x + eps * e_i + eps * e_j)
+                               - f(x + eps * e_i)
+                               - f(x + eps * e_j)
+                               + f(x)) / eps^2,
+        where e_i are coordinate vectors:
+        e_i = (0, 0, ..., 0, 1, 0, ..., 0)
+                          >> i <<
+    """
+    # TODO: Implement numerical estimation of the Hessian
+    return None
